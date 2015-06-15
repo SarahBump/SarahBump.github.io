9d81c1918bbedbdc728bed4bf303d12072534ed0
Sarah Bump <sbump90@gmail.com>
2015-06-15 14:42:37 -0500

    aboutme, contact, header, and footer done

---
+++ js/contact_me.js
@@ -0,0 +1,89 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            // Prevent spam click and default submit behaviour
+            $("#btnSubmit").attr("disabled", true);
+            event.preventDefault();
+
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "././mail/contact_me.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                },
+                cache: false,
+                success: function() {
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+// When clicking on Full hide fail/success boxes
+$('#name').focus(function() {
+    $('#success').html('');
+});
+
+var nouns1 = ['“There is nothing noble in being superior to your fellow man; true nobility is being superior to your former self” –Ernest Hemingway', '“I really do believe that anything in life, any obstacle or challenge, can be made better with humor.”  -Amy Poehler ', '“It’s amazing what you can accomplish when you don’t care who gets credit.” –Harry S. Truman', '“Be a pattern for the world” –Lao Tzu', '“The day will be what you make it, so rise, like the sun, and burn.” – William C. Hannan', '“We have to continually be jumping off cliffs and developing our wings on the way down” –Kurt Vonnegut', '“It does not matter how slowly you go as long as you do not stop.” –Confucius', '“Great things are done by a series of small things brought together” –Vincent Van Gogh']
+
+function random(){
+  var item1 = nouns1[Math.floor(Math.random()*nouns1.length)];
+  $("#quotes").text(item1);
+  // $("#quotes").html(quote)
+  // // $("picture").append ???????
+
+}
+random();
+
+//
+$(document).ready(function(){
+    window.setInterval(random, 9000);
+    })
